c703530329c86ae3ec57fa051b181af1d52ab5e2
alantang <107459091+alantang1977@users.noreply.github.com>
2025-04-25 11:23:15 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,9 @@ ip_version_priority = "ipv6"
 source_urls = [
     #"http://aktv.space/live.m3u",
     "http://92.112.21.169:30000/mytv.m3u",
+    "http://lisha521.dynv6.net.fh4u.org/tv.txt",
+    "https://web.banye.tech:7777/tv/hlj.txt",
+    "https://iptv.catvod.com/tv.m3u",
     "https://gh.tryxd.cn/https://raw.githubusercontent.com/hostemail/cdn/main/live/tv.txt",
     "https://gh.tryxd.cn/https://raw.githubusercontent.com/alantang1977/JunTV/refs/heads/main/output/result.m3u",
     "https://gh.tryxd.cn/https://raw.githubusercontent.com/ssili126/tv/main/itvlist.m3u",
