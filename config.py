969628c1079536641cafdcd84ec4a1048f6f4d00
alantang <107459091+alantang1977@users.noreply.github.com>
2025-04-28 15:53:14 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,7 @@
 ip_version_priority = "ipv6"
 
 source_urls = [
+    "http://zhibo.feylen.top/fltv/js/ku9live.php?tpye=fl.txt",
     "https://gh.tryxd.cn/https://raw.githubusercontent.com/Alan-Alana/IPTV/refs/heads/main/channl.txt",
     "https://gh.tryxd.cn/https://raw.githubusercontent.com/peterHchina/iptv/refs/heads/main/IPTV-V4.m3u",
     "https://gh.tryxd.cn/https://raw.githubusercontent.com/peterHchina/iptv/refs/heads/main/IPTV-V6.m3u",
